5822b22de37266141d8e26ca3293e17618f5d0d6
Marcelo Salinas <chelo336@gmail.com>
2022-03-10 12:11:02 -0400

    publicar post

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,11 @@
  const titulo=document.querySelector("#Titulo")
  const detalle=document.querySelector("#Detalle")
  const form=document.querySelector("#post-form")
+ const post = document.querySelector("#resultado-post");
+
  
  form.addEventListener("submit",event =>{
    event.preventDefault();
-   alert("post publicado "+titulo.value+" "+detalle.value);
+   //alert("post publicado "+titulo.value+" "+detalle.value);
+   post.innerHTML = "<p>" +titulo.value+" "+detalle.value + "</p>";
  })
